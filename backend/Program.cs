ee2f44dfc0c0e607e287b40eff64b03722aca380
msuzun <sevki_uzun53@hotmail.com>
2025-07-19 15:38:37 +0300

    DockerFile eklendi

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,7 @@
 // Program.cs
 using FaceGlassesApi.Models;
 using FaceGlassesApi.Services;
-// Artýk SixLabors.ImageSharp'a veya ilgili DI ayarlarýna gerek yok.
-// using SixLabors.ImageSharp.Web.DependencyInjection;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<GoogleGeminiApiConfiguration>(builder.Configuration.GetSection("GoogleGeminiApi"));
 
 builder.Services.AddHttpClient();
-// GeminiProService'in yeni baðýmlýlýklarýný kullan
+
 builder.Services.AddScoped<GeminiProService>();
 
 
@@ -23,13 +22,6 @@ var app = builder.Build();
 
 
 app.UseHttpsRedirection();
-
-// Statik dosya sunucusu ayarlarý da artýk gerekli deðil (Gözlük resimleri için)
-// app.UseStaticFiles(new StaticFileOptions
-// {
-//     FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "GlassesImages")),
-//     RequestPath = "/glasses-images"
-// });
 
 app.UseAuthorization();
 
